f5df7faa828c6a52693037b491312942f2eb21ce
jaycossey <jaycossey@live.com>
2024-02-29 06:10:14 +0000

    need to check the array state management docs

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleSubmit = () => {
+    event.preventDefault();
+    const nameObj = {name: newName};
+    setPersons((prev) => {
+      [...prev, nameObj]
+    });
+    console.log(persons);
+  }
+
+  const handleChange = (name) => {
+    console.log(persons);
+    setNewName(name);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      {/* important to note (for myself) prevent default runs on the FORM onSubmit, not the button of type submit */}
+      <form onSubmit={handleSubmit}>
+        <div>
+          Name: <input value={newName} onChange={(e) => handleChange(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>Debug: {newName}</div>
+      {persons.map((pers, i) => {
+        return (<p key={i}>{pers.name}</p>);
+      })}
+    </div>
+  )
+}
+
+export default App
