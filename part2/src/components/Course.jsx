577c11d6c09180d24700ded250c1d4f025ef2878
jaycossey <jaycossey@live.com>
2024-02-29 05:16:32 +0000

    finished exercises 2.2 and 2.3

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -1,12 +1,20 @@
 const Course = (props) => {
     console.log(props.course.parts);
     const parts = props.course.parts;
+
+    let total = parts.reduce((s, p) => {
+        return s + p.exercises;
+    }, 0);
+
     return (
         <>
         <h1>{props.course.name}</h1>
+            
             {parts.map((part) => {
-                return (<p>{part.name} {part.exercises}</p>)
+                return (<p key={part.key}>{part.name} {part.exercises}</p>)
             })}
+
+        <p>Total Exercises: {total}</p>
         </>
     );
 }
